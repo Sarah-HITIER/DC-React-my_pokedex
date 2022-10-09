cccd384a5e586cac6d64a4d63104b745c0cdb90d
Sarah HITIER <sarah.hitier@sfr.fr>
2022-10-09 22:10:17 +0200

    feat: app style

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import "./App.scss";
 import { NavBar } from "containers";
+import { styled, Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
+
+const Main = styled("main")(({ theme }) => ({
+    backgroundColor: theme.palette.background.main,
+    minHeight: "100vh",
+    padding: "80px 0",
+    display: "flex",
+    flexDirection: "column",
+    color: theme.palette.text.main,
+    fontSize: theme.palette.fontSize
+}));
 
 function App() {
     return (
         <div className="App">
             <NavBar />
-            <main className="App-main">
-                <Outlet />
-            </main>
+            <Main>
+                <Container>
+                    <Outlet />
+                </Container>
+            </Main>
         </div>
     );
 }
