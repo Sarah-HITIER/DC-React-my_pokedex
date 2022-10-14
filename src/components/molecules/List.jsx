441a3472e21b27bafd355ba2c24d6b66388c9b4a
Sarah HITIER <sarah.hitier@sfr.fr>
2022-10-14 09:40:39 +0200

    feat: number of results and id accepts

--- src/components/molecules/List.jsx
+++ src/components/molecules/List.jsx
@@ -1,26 +1,23 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { ListItem } from "../atoms";
 
-export default function List({ items = [] }) {
-    if (items.length === 0) {
-        return "Items not found";
-    }
+export default function List({ items = [], numberOfItems = 0 }) {
+    if (items.length === 0)
+        return <Typography sx={{ mt: "16px" }}>Items not found</Typography>;
 
     return (
-        <Grid
-            container
-            spacing={4}
-            // justifyContent="space-evenly"
-            alignItems="center"
-            sx={{ mt: 0 }}
-        >
+        <Grid container spacing={4} alignItems="center" sx={{ mt: 0 }}>
             {items.map((item) => {
-                // Item id
-                const paths = item.url.split("/").filter((path) => path !== "");
-                const id = paths[paths.length - 1];
+                let id = item.id;
+                if (!id) {
+                    const paths = item.url
+                        .split("/")
+                        .filter((path) => path !== "");
+                    id = paths[paths.length - 1];
+                }
 
                 return (
-                    <Grid key={id} item xs={6} sm={4} md={3}>
+                    <Grid key={id} item xs={6} sm={3} md={3}>
                         <ListItem id={id} name={item.name}></ListItem>
                     </Grid>
                 );
