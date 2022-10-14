8919a1705c0f2cdd26b8a5c9fca8595787457c3b
Sarah HITIER <sarah.hitier@sfr.fr>
2022-10-14 09:39:37 +0200

    feat: sessionStorage and searchBar

--- src/containers/MainList.jsx
+++ src/containers/MainList.jsx
@@ -1,45 +1,34 @@
-import { List, SearchBar } from "components/molecules";
+import { List, SearchBar, PaginationComponent } from "components/molecules";
 import { Loading } from "components/atoms";
 import { useItems } from "hooks";
 import { useState, useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
-import { styled, Pagination } from "@mui/material";
-
-const StyledPagination = styled(Pagination)(({ theme }) => ({
-    marginTop: theme.spacing(2),
-    "& .MuiButtonBase-root, & .MuiPaginationItem-root": {
-        color: "white"
-    }
-}));
+import { createSearchParams, useSearchParams } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 export default function MainList() {
     const [page, setPage] = useState(1);
     const [countPage, setCountPage] = useState(1);
 
-    const { isLoading, data, error } = useItems(page);
-    const [filteredItems, setFilteredItems] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    const [searchParams] = useSearchParams();
-    const [searchValue, setSearchValue] = useState(searchParams.get("search"));
+    const { isLoading, data, error } = useItems(
+        searchParams.get("search"),
+        page
+    );
+    const [filteredItems, setFilteredItems] = useState([]);
+    const [numberOfItems, setNumberOfItems] = useState(0);
 
     useEffect(() => {
         if (data) {
             setFilteredItems(data.results);
             setCountPage(data.pages);
+            setNumberOfItems(data.count);
         }
     }, [data]);
 
-    // if (!filteredItems || filteredItems.results.length === 0) {
-    //     return "Items not found";
-    // }
-
     const handleChange = (ev) => {
-        setSearchValue(ev.target.value);
-        setFilteredItems(
-            data.results.filter((item) =>
-                item.name.includes(ev.target.value.toLowerCase())
-            )
-        );
+        setPage(1);
+        setSearchParams(createSearchParams({ search: ev.target.value }));
     };
 
     if (isLoading) return <Loading />;
@@ -47,16 +36,16 @@ export default function MainList() {
     return (
         <>
             <SearchBar
-                value={searchValue}
+                value={searchParams.get("search")}
                 handleChange={handleChange}
             ></SearchBar>
+            <Typography sx={{ mt: "16px" }}>{numberOfItems} results</Typography>
             <List items={filteredItems}></List>
-            <StyledPagination
-                count={countPage}
+            <PaginationComponent
+                countPage={countPage}
                 page={page}
-                onChange={(_, value) => setPage(value)}
-                color="primary"
-            />
+                handlePage={(_, value) => setPage(value)}
+            ></PaginationComponent>
         </>
     );
 }
--- src/hooks/useItems.js
+++ src/hooks/useItems.js
@@ -1,17 +1,34 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
-export function useItems(page = 1) {
+const limit = 20;
+
+export function useItems(searchParams = "", page = 1) {
     return useQuery(
-        ["items", page],
+        ["items", page, searchParams],
         async () => {
-            const limit = 20;
+            let results = JSON.parse(sessionStorage.getItem("allItems"));
+            if (!results) {
+                let res = await axios({
+                    url: `https://pokeapi.co/api/v2/pokemon?offset=0&limit=5000`
+                });
+                results = res.data.results;
+                sessionStorage.setItem("allItems", JSON.stringify(results));
+            }
+
+            if (searchParams) {
+                results = results.filter((item) =>
+                    item.name.includes(searchParams.toLowerCase())
+                );
+            }
+
+            const pages = Math.ceil(results.length / limit);
             const offset = limit * (page - 1);
-            let res = await axios({
-                url: `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-            });
-            const pages = Math.ceil(res.data.count / limit);
-            return { pages, results: res.data.results };
+            return {
+                pages,
+                count: results.length,
+                results: results.slice(offset, offset + limit)
+            };
         },
         { keepPreviousData: true }
     );
